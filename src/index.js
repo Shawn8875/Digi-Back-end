09d9b81e1a6bb1ff418fdf38d30e5816891b5d8b
Shawn Cooper <dailymoney101@mail.com>
2026-03-25 16:18:00 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-
-Shawn Cooper <shawncooper289@gmail.com>
-1:44 AM (0 minutes ago)
-to me
-
 import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("DigiMark Backend is running");
 });
+app.use("/api", testRoute);
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
